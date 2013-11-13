9c349885872243d9dd7de2f32b54b5e0057bf1cc
Jan Pieter Waagmeester <jieter@jieter.nl>
2013-11-13 15:39:29 +0100

    automate genning last summary

---
+++ visualize.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python2.7
+
+# Dump file contents to CSV at stdout
+#
+# Fisher and Paykel CPAP .FPH file parser.
+# http://sourceforge.net/apps/mediawiki/sleepyhead/index.php?title=Icon
+
+import os
+from fph.parseFile import parseFile
+
+def lastLog():
+	os.chdir('data/')
+	files = os.listdir('.')
+	files.sort(reverse=True)
+	os.chdir('../')
+	return files[0]
+
+
+f = parseFile('data/' + lastLog() + '/SUM0001.FPH')
+
+
+
+w = open('visualize/summary.csv', 'w')
+w.write(f.toCSV())
