c396512572dfda63f7b22eabc86618587c86b5c5
Jan Pieter Waagmeester <jieter@jieter.nl>
2013-11-15 11:47:02 +0100

    automate workflow

--- visualize.py
+++ visualize.py
@@ -6,7 +6,28 @@
 # http://sourceforge.net/apps/mediawiki/sleepyhead/index.php?title=Icon
 
 import os
+from datetime import date
+import shutil
+
 from fph.parseFile import parseFile
+
+
+def copyUSB():
+	# ubuntu mounts the USB stick on /media/<user>/FPHCARE/
+	srcdir = '/media/%s/FPHCARE/FPHCARE/ICON/' % os.getlogin()
+	if (not os.path.isdir(srcdir)):
+		return
+
+	srcdir += os.listdir(srcdir)[0]
+
+	dstdir = 'data/' + str(date.today())
+	if (os.path.isdir(dstdir)):
+		raise Exception('Directory already exists (%s)' % dstdir)
+
+	os.mkdir(dstdir)
+
+	for srcfile in os.listdir(srcdir):
+		shutil.copy(srcdir + '/' + srcfile, dstdir)
 
 def lastLog():
 	os.chdir('data/')
@@ -15,8 +36,19 @@ def lastLog():
 	os.chdir('../')
 	return files[0]
 
+def lastSummary():
+	last = lastLog()
+	f = parseFile('data/' + last + '/SUM0001.FPH')
 
-f = parseFile('data/' + lastLog() + '/SUM0001.FPH')
+	w = open('site/summary.csv', 'w')
+	w.write(f.toCSV())
 
-w = open('site/summary.csv', 'w')
-w.write(f.toCSV())
+	return last
+
+try:
+	copyUSB()
+	print 'Copied files from FPHCARE InfoUSB'
+except Exception, e:
+	print e.message + '; skipped.'
+
+print 'Generated summary from %s in site/summary.csv' % lastSummary()
